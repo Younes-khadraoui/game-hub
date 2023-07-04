cde31d27348c2db4932aafbd27890977d413ef19
Younes-khadraoui <younes.khadraoui.pro@gmail.com>
2023-07-04 12:05:04 +0100

    Create costum hook to fetch the games

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from 'axios'
 export default axios.create( {
     baseURL:'https://api.rawg.io/api',
     params:{
-        key: '73072acdd6654be1a56d228422e0732e'
+        key: '274558aaabf649d796dd7fdebacdb432'
     }
 })
