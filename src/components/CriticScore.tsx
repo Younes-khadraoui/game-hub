8603bdc364f508719d259adbff678977a3f1fb81
Younes-khadraoui <younes.khadraoui.pro@gmail.com>
2023-07-07 04:05:07 +0100

    Get optimized images

--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -1,7 +1,9 @@
 import {Badge} from '@chakra-ui/react'
+
 interface Props {
     score: number;
 }
+
 const CriticScore = ({score}:Props) => {
     let color = score > 75 ? 'green' : score > 60 ? 'yellow' : ''
   return (
