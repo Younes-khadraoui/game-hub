20710f5dfff12ce61a45f5ef30abd87f31c495a6
Younes-khadraoui <younes.khadraoui.pro@gmail.com>
2023-08-10 15:23:15 +0100

    Build search Input

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -5,16 +5,15 @@ import {
   List,
   ListItem,
   Spinner,
-  Text,
 } from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
-  selectedGenre: Genre|null;
+  selectedGenre: Genre | null;
 }
-const GenreList = ({ onSelectGenre,selectedGenre }: Props) => {
+const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
   if (error) return null;
   if (isLoading) return <Spinner />;
@@ -32,7 +31,7 @@ const GenreList = ({ onSelectGenre,selectedGenre }: Props) => {
               onClick={() => onSelectGenre(genre)}
               fontSize="lg"
               variant="link"
-              fontWeight={genre.id===selectedGenre?.id ? "bold":"normal"}
+              fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
             >
               {genre.name}
             </Button>
